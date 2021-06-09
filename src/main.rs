a03d2df56402db86299a034828260085e042e019
dizzyplay <vovoboss@gmail.com>
2021-06-10 03:26:44 +0900

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
 use std::collections::HashMap;
+use std::env;
+use std::fs::OpenOptions;
 
-mod Symbol{
-    use std::collections::HashMap;
-    #[derive(Debug)]
-    pub struct SymbolTable {
-        table:HashMap<String,usize>
-    }
-
-    impl SymbolTable {
-        pub fn new()->Self{
-            let mut table = HashMap::new();
-            for i in 0..15 {
-                table.insert(format!("R{}",i),i);
-            }
-            SymbolTable{
-                table
-            }
-        }
-        pub fn get(&self, q:&str) -> Some(usize) {
-            self.table.get(q)
-        }
-    }
-}
+mod Symbol;
 
 fn main() {
+    // read file
+    let file_name = env::args().collect::<Vec<String>>();
+    println!("{:?}",file_name);
     let mut symbol_table = Symbol::SymbolTable::new();
 
     println!("{:?}",symbol_table.get("R15"));
 }
 
 
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/06/add/Add.asm
+
+// Computes R0 = 2 + 3  (R0 refers to RAM[0])
+
+// @2
+// D=A
+// @3
+// D=D+A
+// @0
+// M=D
