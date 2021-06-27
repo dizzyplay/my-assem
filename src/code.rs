e6545eca50e3267c425eff985c975b2e7428e6ba
dizzyplay <vovoboss@gmail.com>
2021-06-27 19:04:28 +0900

    add comp

--- src/code.rs
+++ src/code.rs
@@ -14,6 +14,76 @@ fn dest(symbol: String) -> String {
     // https://stackoverflow.com/questions/50458144/what-is-the-easiest-way-to-pad-a-string-with-0-to-the-left
 }
 
+fn comp(symbol:String) -> String {
+    match symbol.as_str() {
+        "0" => format!("0101010"),
+        "1" => format!("0111111"),
+        "-1" => format!("0111010"),
+        "D" => format!("0001100"),
+        "A" => format!("0110000"),
+        "!D" => format!("0001101"),
+        "!A" => format!("0110001"),
+        "-D" => format!("0001111"),
+        "-A" => format!("0110011"),
+        "D+1" => format!("0011111"),
+        "A+1" => format!("0110111"),
+        "D-1" => format!("0001110"),
+        "A-1" => format!("0110010"),
+        "D+A" => format!("0000010"),
+        "D-A" => format!("0010011"),
+        "A-D" => format!("0000111"),
+        "D&A" => format!("0000000"),
+        "D|A" => format!("0010101"),
+        "M" => format!("1110000"),
+        "!M" => format!("1110001"),
+        "-M" => format!("1110011"),
+        "M+1" => format!("1110111"),
+        "M-1" => format!("1110010"),
+        "D+M" => format!("1000010"),
+        "D-M" => format!("1010011"),
+        "M-D" => format!("1000111"),
+        "D&M" => format!("1000000"),
+        "D|M" => format!("1010101"),
+        _ => panic!("wrong symbol")
+    }
+}
+
+#[test]
+fn comp_test(){
+    assert_eq!(comp(format!("0")),"0101010");
+    assert_eq!(comp(format!("1")),"0111111");
+    assert_eq!(comp(format!("-1")),"0111010");
+    assert_eq!(comp(format!("D")),"0001100");
+    assert_eq!(comp(format!("A")),"0110000");
+    assert_eq!(comp(format!("!D")),"0001101");
+    assert_eq!(comp(format!("!A")),"0110001");
+    assert_eq!(comp(format!("-D")),"0001111");
+    assert_eq!(comp(format!("-A")),"0110011");
+    assert_eq!(comp(format!("D+1")),"0011111");
+    assert_eq!(comp(format!("A+1")),"0110111");
+    assert_eq!(comp(format!("D-1")),"0001110");
+    assert_eq!(comp(format!("A-1")),"0110010");
+    assert_eq!(comp(format!("D+A")),"0000010");
+    assert_eq!(comp(format!("A+1")),"0110111");
+    assert_eq!(comp(format!("D-1")),"0001110");
+    assert_eq!(comp(format!("A-1")),"0110010");
+    assert_eq!(comp(format!("D+A")),"0000010");
+    assert_eq!(comp(format!("D-A")),"0010011");
+    assert_eq!(comp(format!("A-D")),"0000111");
+    assert_eq!(comp(format!("D&A")),"0000000");
+    assert_eq!(comp(format!("D|A")),"0010101");
+    assert_eq!(comp(format!("M")),"1110000");
+    assert_eq!(comp(format!("!M")),"1110001");
+    assert_eq!(comp(format!("-M")),"1110011");
+    assert_eq!(comp(format!("M+1")),"1110111");
+    assert_eq!(comp(format!("M-1")),"1110010");
+    assert_eq!(comp(format!("D+M")),"1000010");
+    assert_eq!(comp(format!("D-M")),"1010011");
+    assert_eq!(comp(format!("M-D")),"1000111");
+    assert_eq!(comp(format!("D&M")),"1000000");
+    assert_eq!(comp(format!("D|M")),"1010101");
+}
+
 #[test]
 fn dest_test(){
     assert_eq!(dest(format!("")), "000");
